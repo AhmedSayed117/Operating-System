b65afcdea1beacbefb02214aa0b32bdaef41335d
Ahmed Sayed <62659230+Mido1762001@users.noreply.github.com>
2021-11-05 02:32:15 +0200

    v1.0

---
+++ ass1/src/Parser.java
@@ -0,0 +1,17 @@
+class Parser {
+    String commandName;
+    String[] args;
+
+    public boolean parse(String input){
+        return true;
+    }
+
+    public String getCommandName(){
+        return commandName;
+    }
+
+    public String[] getArgs(){
+
+        return args;
+    }
+}
