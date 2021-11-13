1c9099938a9eb8f8954de5f8c4b44222e0de1308
Ahmed Sayed <62659230+Mido1762001@users.noreply.github.com>
2021-11-13 22:36:26 +0200

    v4.0

--- ass1/src/Terminal.java
+++ ass1/src/Terminal.java
@@ -5,14 +5,21 @@ import java.util.*;
 
 public class Terminal {
     Parser parser;
+    public int checkCount=0;
+    public int checkCount2=0;
     private String path ;
 
     public Terminal(){
         this.path = System.getProperty("user.dir");
     }
     //1 mark
-    public void echo(String e){
-        System.out.println(e);
+    public String echo(String e){
+        if( checkCount2==0 ){
+            System.out.println(e);
+            return e;
+        }
+        checkCount2 = 0;
+        return e;
     }
     //1 mark
     int c=0;
@@ -69,17 +76,28 @@ public class Terminal {
 //        path = System.getProperty("user.home");
     }
     //تصاعدي
-    public void ls(){
+    public String ls(){
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
-
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("---------------------------------\n\tFile\nFile name -> : " + listOfFiles[i].getName()+"\nFile path -> : "+ listOfFiles[i].getPath()+"\n----------------------------------");
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("---------------------------------\n\tFolder\nFolder name -> : " + listOfFiles[i].getName()+"\nFolder path -> : "+ listOfFiles[i].getPath()+"\n----------------------------------");
+        String content="";
+        if(checkCount==0){
+            for(int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    System.out.println("---------------------------------\n\tFile\nFile name -> : " + listOfFiles[i].getName()+"\nFile path -> : "+ listOfFiles[i].getPath()+"\n----------------------------------");
+                } else if (listOfFiles[i].isDirectory()) {
+                    System.out.println("---------------------------------\n\tFolder\nFolder name -> : " + listOfFiles[i].getName()+"\nFolder path -> : "+ listOfFiles[i].getPath()+"\n----------------------------------");
+                }
+                content+=listOfFiles[i].getName();
+                content+="\n";
+            }
+        }else {
+            for (int i = 0; i < listOfFiles.length; i++) {
+                content+=listOfFiles[i].getName();
+                content+="\n";
             }
         }
+        checkCount=0;
+        return content;
     }
     //تنازلي
     public void lsr(){
@@ -283,6 +301,106 @@ public class Terminal {
         }
     }
 
+    public void rm(String s){
+        File file = new File(path+ "\\" +s);
+        if(file.exists()){
+            file.delete();
+            echo(s + " deleted Successfully");
+        }else {
+            echo(s + " Not Found!");
+        }
+
+    }
+
+    public void cat(String s1) throws IOException {
+        FileInputStream file = new FileInputStream(path + "\\"+ s1);
+        int i=0;String s="";
+        while ((i=file.read())!=-1){
+            s+=(char)i;
+        }
+        echo(s);
+    }
+
+    public void cat(String s1,String s2) throws IOException {
+        File f1 = new File(path + "\\"+ s1);
+        File f2 = new File(path + "\\"+ s2);
+
+        int i=0,j=0;String s="";
+        if(f1.exists() && f2.exists()){
+            FileInputStream file1 = new FileInputStream(path + "\\"+ s1);
+            FileInputStream file2 = new FileInputStream(path + "\\"+ s2);
+
+            while ((i=file1.read())!=-1){
+                s+=(char)i;
+            }
+            s+='\n';
+            file1.close();
+            while ((j=file2.read())!=-1){
+                s+=(char)j;
+            }
+            file2.close();
+            echo(s);
+        }else{
+            echo("not available files");
+        }
+    }
+
+    public void write(Terminal obj,String s,String file,String e) throws IOException {
+        checkCount = 1;
+        checkCount2 = 1;
+        File f1 = new File(path + "\\"+file);
+        if (f1.exists()){
+            FileOutputStream out = new FileOutputStream(f1);
+            if(s.equals("pwd")){
+                byte[] output = obj.pwd().getBytes();
+                out.write(output);
+            }else if (s.equals("ls")){
+                byte[] output = obj.ls().getBytes();
+                out.write(output);
+            }else if(s.equals("echo")){
+                byte[] output = obj.echo(e).getBytes();
+                out.write(output);
+            }
+        }else {
+            File f = new File(path + "\\"+file);
+            f.createNewFile();
+        }
+    }
+
+    public void writeAppend(Terminal obj,String s,String file,String e) throws IOException {
+        checkCount = 1;
+        checkCount2 = 1;
+
+        FileInputStream fin = new FileInputStream(path +"\\"+file);
+        int i=0;String str="";
+        while((i=fin.read())!=-1){
+            str+=(char)i;
+        }
+        FileOutputStream fout = new FileOutputStream(path +"\\"+file);
+        File f1 = new File(path + "\\"+file);
+        if (f1.exists()){
+
+            String ob1 = obj.pwd();
+            String ob2 = obj.ls();
+            String ob3 = obj.echo(e);
+
+            String fn1 = str +"\n"+ ob1;
+            String fn2 = str + "\n"+ob2;
+            String fn3 = str +"\n"+ ob3;
+
+            if(s.equals("pwd")){
+                fout.write(fn1.getBytes());
+            }else if (s.equals("ls")){
+                fout.write(fn2.getBytes());
+            }else if(s.equals("echo")){
+                fout.write(fn3.getBytes());
+            }
+        }else{
+            File f = new File(path + "\\"+file);
+            f.createNewFile();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Terminal T = new Terminal();
         Scanner input = new Scanner(System.in);
@@ -315,6 +433,8 @@ public class Terminal {
                     T.lsr();
                     break;
                 default:
+//                    T.write(T,"echo","test.txt","sfhsdfgjhsdfgsdhgjhsdfgj");
+                    //cat countSpace
                     //cd ahmed
                     if(options.charAt(0)=='c' && options.charAt(1)== 'd' && options.charAt(2)== ' '){
                         String p;
@@ -364,36 +484,95 @@ public class Terminal {
                         File f1 = new File(f);
                         File f2 = new File(p);
                         T.cp(f1,f2);
-                    }
+                    }//rm ahmed.txt
+                    else if(options.charAt(0)=='r' && options.charAt(1)=='m' && options.charAt(2)==' '){
+                        String s = options.substring(3);
+                        T.rm(s);
+                    }//cat ahmed
+                    else if(options.charAt(0)=='c' && options.charAt(1)=='a' && options.charAt(2)=='t'){
+                        String ch = options.substring(4);
+                        int countSpace = 0;String str="";
+                        for (int i=0;i<ch.length();i++){
+                            if (ch.charAt(i)==' ') countSpace++;
+                        }
+                        if(countSpace==0){
+                            T.cat(ch);
+                        }//cat ahmed sayed
+                        else{
+                            int i=0;String str1="",str2="";
+                            while (ch.charAt(i)!=' '){
+                                str1+=ch.charAt(i);
+                                i++;
+                            }i++;
+                            while (ch.charAt(i)!=' '){
+                                str2+=ch.charAt(i);
+                                i++;
+                                if(i==ch.length())break;
+                            }
+                            T.cat(str1,str2);
+                        }
+                        //pwd > file
+                        //ls > file
+                        //echo ahmed > file
+                        //ahmed sayed > file
+                    }else if(options.contains(">")){
+                        int index = options.indexOf('>');
+                        if(options.charAt(index+1)=='>'){
+                            if(options.contains("pwd")){
+                                String sub = options.substring(7);
+                                T.writeAppend(T,"pwd",sub,"");
+                            }
+                            else if(options.contains("ls")){
+                                String sub = options.substring(6);
+                                T.writeAppend(T,"ls",sub,"");
+                            }
+                            else if(options.contains("echo")){
+                                options=options.substring(5);
+                                String str="";
+                                int i=0;
+                                for (;options.charAt(i)!='>' && options.charAt(i+1)!='>' && i!=options.length()-1 ;i++){
+                                    str+=options.charAt(i);
+                                }
+                                i+=4;
+                                String sub = options.substring(i);
+                                T.writeAppend(T,"echo",sub,str);
+                            }else{
+                                T.echo("invalid command!");
+                            }
+                        }
+                        else {
+                            if(options.contains("pwd")){
+                                String sub = options.substring(6);
+                                T.write(T,"pwd",sub,"");
+                            }
+                            else if(options.contains("ls")){
+                                String sub = options.substring(5);
+                                T.write(T,"ls",sub,"");
+                            }
+                            else if(options.contains("echo")){
+                                options=options.substring(5);
+                                String str="";
+                                int i=0;
+                                for (;options.charAt(i)!='>';i++){
+                                    str+=options.charAt(i);
+                                }
+                                i+=2;
+                                String sub = options.substring(i);
+                                T.write(T,"echo",sub,str);
+                            }else{
+                                T.echo("invalid command!");
+                            }
+                        }
+
+                    }//pwd >> file
+                    //ls >> file
+                    //echo ahmed >> file
+                    //ahmed sayed >> file
                     else{
                         T.echo("invalid command!");
                     }
 
                     }
-
         }
-
-//        Terminal T = new Terminal();
-//        T.echo(T.pwd());
-//        T.echo(T.pwd());
-//        String s;
-//        s = input.nextLine();
-//        T.setPath(s);
-//        T.echo(T.getPath());
-//        T.echo(T.getPath());
-//        String[] a = new String[100];
-//        a[0]=".";
-//        a[1]=".";
-//        T.cd(a);
-//        T.cd(a);
-//        T.cd(a);
-//        T.cd(a);
-//        a[0] = "ahmed\\sayed\\hassan";
-//        T.cd(a);
-//       System.out.println( T.getPath());
-
-//        String b ="ahmed";
-//        System.out.println(b.charAt(4));
-
     }
 }
